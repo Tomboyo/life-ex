0e9be5b5bfb9dfb999cda76baa9151a9ee42f1c3
Tom Simmons (Tomboyo) <tomasimmons@gmail.com>
2018-12-02 04:52:46 -0500

    Add some @doc comments

--- lib/life.ex
+++ lib/life.ex
@@ -1,4 +1,11 @@
+@moduledoc """
+Defines functions to play Conway's Game of Life.
+"""
 defmodule Life do
+  @doc """
+  Create a new grid based on the given grid and the normal rules for Conway's
+  Game of Life.
+  """
   def advance_grid(grid) do
     for { point, cell } <- grid,
         into: %{},
@@ -8,6 +15,9 @@ defmodule Life do
         }
   end
 
+  @doc """
+  Counts the number of cells in the given array which are :alive
+  """
   def count_alive(cells) do
     Enum.reduce(cells, 0, fn cell, acc ->
         case cell do
@@ -17,6 +27,11 @@ defmodule Life do
       end)
   end
 
+  @doc """
+  Returns a list of cells which neighbor the given point in one of eight
+  cardinal directions. At the edges of the map, where there are no neighbors,
+  the resulting may contain fewer than 8 cells.
+  """
   def neighbors(grid, point) do
     [ {-1, -1}, {0, -1}, {1, -1},
       {-1,  0},          {1,  0},
@@ -27,10 +42,13 @@ defmodule Life do
       |> Enum.filter(fn cell -> cell != nil end)
   end
 
+  @doc """
+  Given a cell and the number of its living neighbors, return the next state of
+  the cell (:alive or :dead)
+  """
   def advance_cell(:alive, 2), do: :alive
   def advance_cell(:alive, 3), do: :alive
   def advance_cell(:alive, _), do: :dead
-  
   def advance_cell(:dead, 3), do: :alive
   def advance_cell(:dead, _), do: :dead
 end
--- lib/point.ex
+++ lib/point.ex
@@ -1,7 +1,13 @@
+@moduledoc """
+A cartesian coordinate pair
+"""
 defmodule Point do
   @enforce_keys [:x, :y]
   defstruct [:x, :y]
 
+  @doc """
+  Get the point translated by delta from the origin.
+  """
   def translate(origin, delta) do
     %Point{
       x: origin.x + delta.x,
